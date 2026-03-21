a5144cf4a243f38f2bd72dce857ec214bd512674
thenamelessdev <nameless@thenamelessdev.com>
2026-03-21 21:47:08 +0100

    update: change text on main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function MainPage() {
   return (<>
-    <p className='text-lg font-bold'>Hello world!</p>
+    <p className='text-lg font-bold'>Coming soon!</p>
   </>)
 }
