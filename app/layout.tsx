8a6d8b50311ef14d73969d93ef77ffccf57ff17f
thenamelessdev <nameless@thenamelessdev.com>
2026-03-22 12:11:01 +0100

    feat: add projects page and change website color to darker

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,19 @@ import "./globals.css";
 import Link from 'next/link';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-  const pageClass = "font-bold text-lg cursor-pointer transition-all duration-300 hover:text-gray-500";
+  const pageClass = "text-white font-bold text-lg cursor-pointer transition-all duration-300 hover:text-gray-500";
   return (
     <html lang='en'>
-      <body>
-        <nav className='flex border-b-2 p-3 space-x-5'>
-          <Link href="/" className='text-2xl font-bold hover:text-gray-500 duration-300 transition-all'>Nameless Productions</Link>
+      <body className='bg-gray-800'>
+        <nav className='flex border-b p-3 space-x-5 border-white bg-black'>
+          <Link href="/"><p className='text-white text-2xl font-bold hover:text-gray-500 duration-300 transition-all'>Nameless Productions</p></Link>
 
-          <ul className='ml-auto'>
-            <li className={pageClass}><Link href="/">Home</Link></li>
-          </ul>
+          <div className='ml-auto flex space-x-5'>
+            <Link href="/"><p className={pageClass}>Home</p></Link>
+            <Link href="/projects" className={pageClass}>Projects</Link>
+          </div>
         </nav>
-        <main className='m-3'>
+        <main className='m-3 text-white'>
           {children}
           <br />
           <p>&copy; {new Date().getFullYear()} Nameless Productions. Licensed under the MIT license</p>
---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ProjectsPage() {
+  return (<>
+    <ul className='list-disc list-inside'>
+        <li>Coming soon!</li>
+    </ul>
+  </>)
+}
