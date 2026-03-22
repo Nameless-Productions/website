c62ec76c15b8c4f6f3439424182cd42d3863a991
thenamelessdev <nameless@thenamelessdev.com>
2026-03-22 18:13:39 +0100

    update: styles for hyperlinks

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function ProjectsPage() {
   return (<>
     <ul className='list-disc list-inside'>
-        <li><Link href="/projects/cursed-calculator">Cursed calculator</Link></li>
+        <li><Link href="/projects/cursed-calculator" className="underline text-blue-500">Cursed calculator</Link></li>
     </ul>
   </>)
 }
